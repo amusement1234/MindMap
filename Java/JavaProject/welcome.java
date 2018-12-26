e21a125505a3b0546e9180ea7031e1fcda7e657b
Michael <amusement1992@live.cn>
2018-12-26 10:50:52 +0800

    Signed-off-by: Michael <amusement1992@live.cn>

--- Java/JavaProject/welcome.java
+++ Java/JavaProject/welcome.java
@@ -1,6 +1,30 @@
-public class welcome{
-		public static void main(String[] args)
-		{
-			System.out.println("hello world!");
+import java.util.*;
+
+public class welcome {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int num = 0, temp;
+		do {
+			System.out.print("请输入一个4位正整数：");
+			num = s.nextInt();
+		} while (num < 1000 || num > 9999);
+		int a[] = new int[4];
+		a[0] = num / 1000;
+		a[1] = (num / 100) % 10;
+		a[2] = (num / 10) % 10;
+		a[3] = num % 10;
+		for (int j = 0; j < 4; j++) {
+			a[j] *= 2;
+			a[j] %= 10;
 		}
+		for (int j = 0; j <= 1; j++) {
+			temp = a[j];
+			a[j] = a[2 + j];
+			a[2 + j] = temp;
+		}
+		System.out.print("加密后的数字为：");
+		for (int j = 0; j < 4; j++)
+			System.out.print(a[j]);
+
+	}
 }
